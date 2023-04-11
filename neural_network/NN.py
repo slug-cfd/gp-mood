860cefd44069bb45966bcdbe0f0a52b8e08eb5f0
remi.bourgeois <remi.bourgeois@cea.fr>
2023-04-10 22:39:23 -0700

    new neural network, muuuuch simpler thanks to hdf5

---
+++ neural_network/NN.py
@@ -0,0 +1,84 @@
+from utils import *
+
+class radius_picker(nn.Module):
+
+    def __init__(self, max_radius, nb_layers, hidden_layer_sizes):
+        self.init_parameters = max_radius, nb_layers, hidden_layer_sizes
+
+        super(radius_picker, self).__init__()
+
+        self.max_radius=max_radius
+
+        if (max_radius == 1):
+            self.stencil_size = 13 #3rd order
+        else:
+            print("Error, not programmed")
+            sys.exit()
+        
+        #Input size = stencil * nbvar + nbvar normalisation factors + CFL
+        self.input_size=self.stencil_size*nbvar + nbvar + 1 
+        #Output size = max radius + 1
+        self.output_size=max_radius+1 
+
+        if (len(hidden_layer_sizes)!=nb_layers-2):
+            print(colors.red+"Error, expected", nb_layers-2, "layer sizes, got", len(hidden_layer_sizes))
+            sys.exit()
+
+        self.layers=nn.ModuleList()
+        self.nb_layers=nb_layers
+    
+
+        self.layers.append(  nn.Linear(self.input_size, hidden_layer_sizes[0])  )
+
+        for k_layer in range(1,nb_layers-2):
+            
+            self.layers.append(  nn.Linear(hidden_layer_sizes[k_layer-1], hidden_layer_sizes[k_layer])  )
+
+        self.layers.append(  nn.Linear(hidden_layer_sizes[-1], self.output_size)  )
+
+    def forward(self, x, verbose=False): 
+    #Feed forward function
+        for k_layer,layer in enumerate(self.layers):
+            
+            #layer evaluation
+            if (verbose):
+                print(k_layer,"input_size = ",x.shape)
+            x=layer(x) 
+
+            if (verbose):
+                print(k_layer,"output_size = ",x.shape)
+
+            if (k_layer<self.nb_layers-2): 
+                #Sigmoid except for last layer
+                x=torch.sigmoid(x)
+                
+                if (verbose):
+                    print(k_layer, "sigmoid")
+
+        return x
+    
+    def save(self, file='NN.pt'):
+        torch.save(self.state_dict(), file)
+
+    def load(self, file):
+        self.load_state_dict(torch.load(file))
+
+def unitary_check_NN_class():
+
+    NN1=radius_picker(max_radius=1, nb_layers=4, hidden_layer_sizes=[60,60])
+
+    batch_size=2
+    data=torch.rand((batch_size, NN1.input_size)) #some random data
+    out1=NN1.forward(data, verbose=True)
+    print("NN output =", out1) #print the result of the NN of that data
+
+    #testing save and load
+    NN1.save("backup.pt") #saving NN
+    NN2=radius_picker(*NN1.init_parameters) #Declaring a new NN with the same parameter
+    print("NN2 output before loading NN1 =", NN2.forward(data)) 
+    NN2.load("backup.pt")
+    out2=NN2.forward(data)
+    print("NN2 output after loading NN1 = ",out2, "should be the same as NN1")
+    print("difference is", torch.max(out1-out2).item())
+
+#unitary_check_NN_class()
---
+++ neural_network/utils.py
@@ -0,0 +1,89 @@
+import os
+import random
+import sys
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, random_split
+import random
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+import pickle
+import matplotlib.ticker as ticker
+import math 
+import numpy as np
+import h5py
+
+L=57
+nbvar=4
+torch.set_default_dtype(torch.float32) 
+
+def sign(x):
+    return int(math.copysign(1,x))
+
+dir='../gp-mood-code/'
+
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class MyDataset(Dataset):
+    def __init__(self, dataset_file_hdf5):
+
+        f = h5py.File(dataset_file_hdf5)
+
+        self.inputs = torch.from_numpy(np.array(f['inputs']))
+        self.labels = torch.from_numpy(np.array(f['labels']))
+
+    def __len__(self):
+        return len(self.inputs)
+    
+    def __getitem__(self, index):
+        return self.inputs[index], self.labels[index]
+
+def format(x):
+    """Format a double with one digit after the comma"""
+    return f"{x:.5f}"
+
+
+def plot_loss(epoch, lr, tr_loss, te_loss, length, model_name):
+    fig, ax1 = plt.subplots()
+
+    # plot the loss on the left y-axis
+    ax1.plot(epoch, tr_loss, color='blue', linestyle='dotted', label='Training')
+    ax1.plot(epoch, te_loss, color='blue', label='Testing')
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Losses', color='blue')
+    ax1.tick_params(axis='y', labelcolor='blue')
+    ax1.set_yscale('log')
+    ax1.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:.16g}'.format(y)))
+    ax1.legend(loc='upper left')
+
+    # create a second y-axis on the right
+    ax2 = ax1.twinx()
+
+    # plot the learning rate on the right y-axis
+    ax2.plot(epoch, lr, color='red')
+    ax2.set_ylabel('Learning Rate', color='red')
+    ax2.tick_params(axis='y', labelcolor='red')
+    ax2.set_yscale('log')
+    ax2.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:.16g}'.format(y)))
+
+    plt.title('L = ' + str(length))
+
+    plt.savefig('losses_'+model_name+'_epoch_L_' + str(length) + '.png', dpi=300)
+    plt.clf()
+    ax1.cla()
+    ax2.cla()
+    plt.close(fig)
+
+
+
