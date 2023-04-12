46e0951f82b752f741afa81dd0105ff50ff0737c
remi.bourgeois <remi.bourgeois@cea.fr>
2023-04-12 12:08:55 -0700

    update plotter

--- gp-mood-code/plot/plotter_diag.py
+++ gp-mood-code/plot/plotter_diag.py
@@ -3,12 +3,17 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+def sum_char(input):
+    r=''
+    for char in input:
+        r=r+str(char)[-2]
+    return r
+
 file = sys.argv[1]
 f = h5py.File(file)
 
 time = np.array(f['time'])
 pct_detected_cell = np.array(f['pct_detected_cell'])
-
 running_avg_pct_detected_cell = []
 n_avg=10
 for k in range (n_avg, np.size(time)):
@@ -19,7 +24,12 @@ plt.plot(time[n_avg:],running_avg_pct_detected_cell, color='blue', label='runnin
 
 plt.xlabel('time')
 plt.ylabel('pct detected cells')
-plt.title(file[:-3])
+
+tail=''
+if sum_char(f["method"][0:10]) == "NN_GP_MOOD" :
+    print("yo")
+    tail='\n count_steps_NN_produced_NAN='+str(f["count_steps_NN_produced_NAN"][0])
+plt.title(file[18:-3]+tail)
 plt.legend()
 plt.savefig(file[:-3]+'.png',figsize=(8, 6), dpi=100)
 plt.close()
