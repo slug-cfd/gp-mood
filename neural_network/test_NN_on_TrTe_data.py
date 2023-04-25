63614ef6cb2615b2f3e2e56ee68f3298e0edf4ba
remi.bourgeois <remi.bourgeois@cea.fr>
2023-04-25 12:24:22 -0700

    set NN to eval mode before evaluating

--- neural_network/test_NN_on_TrTe_data.py
+++ neural_network/test_NN_on_TrTe_data.py
@@ -4,19 +4,15 @@ import os
 #Load the NN
 
 lenght_list=range(10,101,10)
-#dataset = MyDataset('mixed_2DRP3_sedov_shu_osher.h5')
-#dataset = MyDataset('dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5')
 dataset = MyDataset('dataset_output_sedov_GP_MOOD_CFL_0.8_256_256.h5')
-#dataset = MyDataset('dataset_output_shu_osher_GP_MOOD_CFL_0.8_256_256.h5')
 
 size=int(len(dataset))
 dataset = DataLoader(dataset=dataset, batch_size=size, shuffle=False)
 
 for lenght in lenght_list:
     NN=radius_picker(max_radius=1, nb_layers=4, hidden_layer_sizes=[lenght,lenght], softmax=True)
-
     NN.load("model_mixed_2DRP3_sedov_shu_osher_"+str(lenght)+'.pt')
-
+    NN.train(mode=False)
     for batch_idx, (data, labels) in enumerate(dataset):
         outputs=NN.forward(data)
 
