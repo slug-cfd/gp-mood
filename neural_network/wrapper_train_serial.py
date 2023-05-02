9fa160801e5419682c092ee595274a76f01a95d9
remi.bourgeois <remi.bourgeois@cea.fr>
2023-05-02 11:27:44 -0700

    fix typo

--- neural_network/wrapper_train_serial.py
+++ neural_network/wrapper_train_serial.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     dataset_file=sys.argv[1]
     model_name=sys.argv[2]
     lenght=int(sys.argv[3])
-    gpu_id=int(sys.argv[4])
-    loss_func=sys.argv[5]
+    loss_func=sys.argv[4]
+    gpu_id=int(sys.argv[5])
 
     train(lenght, dataset_file, model_name, loss_func=loss_func, gpu_id=gpu_id)
