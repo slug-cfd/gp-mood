e1156d8dc41076c266be3e0177a8184ceadaadea
remi.bourgeois <remi.bourgeois@cea.fr>
2023-05-08 12:22:26 -0500

    setup for automation

--- neural_network/wrapper_train_multi_cpu.py
+++ neural_network/wrapper_train_multi_cpu.py
@@ -1,37 +1,22 @@
 from train_function import train
 from utils import *
 
-
 if __name__ == '__main__':
 
+    problem=sys.argv[1]
+    resolution = sys.argv[3]+"_"+sys.argv[3]
+    ntrain=int(sys.argv[2])
     #Amount of core to share the training
-    ncores=cpu_count(logical=False)-1
+    ncores=cpu_count(logical=False)
     print(colors.HEADER+' == Initializing the hyperparameter study on'+colors.green, ncores, colors.HEADER+'cores =='+colors.ENDC)
     #List if NN lenght we want to study
+
+    dataset="../gp-mood-code/dataset_output_"+problem+"_GP_MOOD_CFL_0.8_"+resolution+".h5"
     param_list=[]
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_CEL_no_dropout_0", "CEL", 0))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_CEL_no_dropout_1", "CEL", 0))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_CEL_no_dropout_2", "CEL", 0))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_CEL_no_dropout_3", "CEL", 0))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_CEL_no_dropout_4", "CEL", 0))
+
+    for k in range(0, ntrain+1):
+        param_list.append((20, dataset, problem+"_first_10%_CEL_dropout_0.1_"+str(k), "CEL", 0.1))
  
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_CEL_dropout_0", "CEL", 0.05))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_CEL_dropout_1", "CEL", 0.05))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_CEL_dropout_2", "CEL", 0.05))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_CEL_dropout_3", "CEL", 0.05))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_CEL_dropout_4", "CEL", 0.05))
-
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_MSE_no_dropout_0", "MSE", 0))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_MSE_no_dropout_1", "MSE", 0))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_MSE_no_dropout_2", "MSE", 0))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_MSE_no_dropout_3", "MSE", 0))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_MSE_no_dropout_4", "MSE", 0))
-
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_MSE_dropout_0", "MSE", 0.05))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_MSE_dropout_1", "MSE", 0.05))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_MSE_dropout_2", "MSE", 0.05))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_MSE_dropout_3", "MSE", 0.05))
-    param_list.append((20, "../gp-mood-code/dataset_output_2DRP3_GP_MOOD_CFL_0.8_256_256.h5", "2DRP3_first_100_MSE_dropout_4", "MSE", 0.05))
 
     print("List of hyperparameters to be shared: ", [i for i in param_list], "i.e"+colors.green, len(param_list),colors.ENDC,'elements')
 
